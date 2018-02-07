545baa27946b9dd0fac390f7f2ed1314d0b83125
Erica Stone <erica.stone313@gmail.com>
2018-02-07 14:27:50 -0700

    playerSchema w/out bcrypt done

--- models/player.js
+++ models/player.js
@@ -1,4 +1,7 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    bcrypt = require(bcrypt),
+    SALT_WORK_FACTOR = 10;
 
 const PlayerSchema = new mongoose.Schema ({
 
@@ -9,5 +12,23 @@ const PlayerSchema = new mongoose.Schema ({
     password: {
         type: String,
         required: true
+    },
+    totalPoints: {
+        type: Number,
+        default: 0
+    },
+    totalAttempts: {
+        type: Number,
+        default: 0
+    },
+    scorePerct: {
+        type: Number,
+        default: 0
+    },
+    playerRank: {
+        type: Number,
+        default: null
     }
 })
+
+module.exports = mongoose.model(PlayerSchema)
