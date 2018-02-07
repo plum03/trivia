eff49fe6a13f4d09473a063cdae40a758730b73b
Erica Stone <erica.stone313@gmail.com>
2018-02-06 17:16:23 -0700

    this trivia app is going full stack :)

---
+++ client/src/redux/question.js
@@ -0,0 +1,32 @@
+import axios from 'axios'
+
+const rootUrl = "https://qriusity.com/v1/categories/"
+const questUrl = "/questions"
+
+export function getQuestions(catId) {
+    return dispatch => {
+        axios
+            .get(rootUrl + catId + questUrl)
+            .then((response) => {
+                dispatch({
+                    type: "GET_QUESTIONS", 
+                    questions: response.data
+                })
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+    } 
+}
+
+export default function questReducer(questions = {loading: true, data: []}, action) {
+    switch (action.type) {
+        case "GET_QUESTIONS":
+            return {
+                loading: false,
+                data: action.questions
+            }
+        default:
+            return questions
+    }
+}
